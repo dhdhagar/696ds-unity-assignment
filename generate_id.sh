132574a9cb51b4fb7e08609f97b7719bbcfd5637
Wenlong Zhao <wenlong.zhao.97@gmail.com>
2024-02-01 14:57:21 -0500

    Add instructions

--- generate_id.sh
+++ generate_id.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH -c 2  # Number of CPU cores
-#SBATCH --mem=2GB  # Requested Memory
 #SBATCH -p gypsum-1080ti  # Partition
 #SBATCH -G 1  # Number of GPUs
-#SBATCH -o generate_id-%j.out  # Specify where to save terminal output, where %j = job ID
+#SBATCH -c 2  # Number of CPU cores
+#SBATCH --mem=50GB  # Requested Memory
+#SBATCH -t 0-01:00:00  # Zero day, one hour
+#SBATCH -o generate_id-%j.out  # Specify where to save terminal output, %j = job ID will be filled by slurm
 
 python test/generate_id.py
