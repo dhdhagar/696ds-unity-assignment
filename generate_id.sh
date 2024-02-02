884f07422629e2d90ee9dcc33f5e209f2b4862c2
Wenlong Zhao <wenlong.zhao.97@gmail.com>
2024-02-01 22:21:09 -0500

    load conda

--- generate_id.sh
+++ generate_id.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH -p gypsum-1080ti  # Partition
+#SBATCH -p gypsum-titanx  # Partition
 #SBATCH -G 1  # Number of GPUs
 #SBATCH -c 2  # Number of CPU cores
 #SBATCH --mem=50GB  # Requested Memory
 #SBATCH -t 0-01:00:00  # Zero day, one hour
 #SBATCH -o generate_id-%j.out  # Specify where to save terminal output, %j = job ID will be filled by slurm
 
+module load miniconda/22.11.1-1
+conda activate 696hw1
 python test/generate_id.py
