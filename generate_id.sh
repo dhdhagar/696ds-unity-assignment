fbca35eba5d4a53c40d36fbfd0d8e999453b7b79
Dhruvesh <4356534+dhruvdcoder@users.noreply.github.com>
2024-02-05 17:54:48 -0500

    Update generate_id.sh
    -p gpu --mem=30GB

--- generate_id.sh
+++ generate_id.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p gypsum-titanx  # Partition
+#SBATCH -p gpu  # Partition
 #SBATCH -G 1  # Number of GPUs
 #SBATCH -c 2  # Number of CPU cores
-#SBATCH --mem=50GB  # Requested Memory
+#SBATCH --mem=30GB  # Requested Memory
 #SBATCH -t 0-01:00:00  # Zero day, one hour
 #SBATCH -o generate_id-%j.out  # Specify where to save terminal output, %j = job ID will be filled by slurm
 
